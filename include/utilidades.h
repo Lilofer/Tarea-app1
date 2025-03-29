7f84e2181c24877ea4907fc5ca66b53efdf06e51
Camila Cotapos <camilacotaposmz@gmail.com>
2025-03-29 17:08:59 -0300

    Utilidades h.
    algo de utilidades

---
+++ include/utilidades.h
@@ -0,0 +1,7 @@
+#ifndef UTILIDADES_H
+  #define UTILIDADES_H
+
+  int comparar_fecha(const char* date1, const char* date2);
+  void trim_string(char *str);
+
+  #endif
---
+++ src/utilidades.c
@@ -0,0 +1,10 @@
+#include "utilidades.h"
+
+int comparar_fecha(const char *date1, const char *date2)
+{
+    return 0;
+}
+
+void trim_string(char *str)
+{
+}
