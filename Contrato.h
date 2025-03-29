9f9de35e3a811662240bacf3f735e390478e57d1
Ivanalmonacidh <ialmonacid@alumnos.uai.cl>
2025-03-29 08:41:40 -0300

    Comentarios inciales (nada la vdd)
    Creacion del archivo .h y comentarios superfluos pero que ayudan a la guia

---
+++ Contrato.h
@@ -0,0 +1,2 @@
+//Este es el archivo de contrato, donde especificamos las firmas de las funciones
+//Creo que se hacian otras cosas pero tengo fe
--- tarea1_paradigma.c
+++ tarea1_paradigma.c
@@ -1,1 +1,3 @@
 #include <stdio.h>
+//Empezare por hacer las funciones basicas, no se donde esta el .CSV que tenemos que usar tho
+//Recuerden que hay que entregar un .exe C al correr el codigo genera el .exe por si solo 
