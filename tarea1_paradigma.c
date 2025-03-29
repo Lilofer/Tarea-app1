a41deae22427d789bd61a6ea3b54b83d88b8069d
Ivanalmonacidh <ialmonacid@alumnos.uai.cl>
2025-03-29 09:12:37 -0300

    Bibliotecas a requerir
    Tambien maximos de lectura del CSV

--- tarea1_paradigma.c
+++ tarea1_paradigma.c
@@ -1,3 +1,9 @@
-#include <stdio.h>
 //Empezare por hacer las funciones basicas, no se donde esta el .CSV que tenemos que usar tho
 //Recuerden que hay que entregar un .exe C al correr el codigo genera el .exe por si solo 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+#define MAX_LINE 1024 //maximo de 1024 cracateres por fila
+#define MAX_ORDERS 1000 //maximo 1000 filas 
